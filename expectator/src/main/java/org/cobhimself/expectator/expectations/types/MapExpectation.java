08078006288dfe3c6b013c55809cd6469eec0aa8
Collin Brooks <cbrooks@sentryds.com>
2021-10-17 22:17:04 -0500

    Add MapExpectation

---
+++ expectator/src/main/java/org/cobhimself/expectator/expectations/types/MapExpectation.java
@@ -0,0 +1,254 @@
+package org.cobhimself.expectator.expectations.types;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A <code>MapExpectation</code> is used to provide expectations for objects which implement
+ * the <code>Map</code> interface.
+ */
+public class MapExpectation extends Expectation<Map<?,?>> {
+
+  /**
+   * Construct a <code>MapExpectation</code> with a known expected value up front.
+   *
+   * @param expectedValue the value our expectation expects
+   * @param name          the name of the <code>MapExpectation</code>
+   */
+  protected MapExpectation(Map<?,?> expectedValue, String name) {
+    super(expectedValue, name);
+  }
+
+  /**
+   * Construct a <code>MapExpectation</code> without specifying the expected value up front.
+   *
+   * @param name the name of the <code>MapExpectation</code>
+   */
+  protected MapExpectation(String name) {
+    super(name);
+  }
+
+  /**
+   * Expect our expected map's keys to contain the provided actual value.
+   *
+   * @param actual the key we expect to be contained within our expected map's keys collection
+   *
+   * @return self
+   */
+  public MapExpectation expectKeysContain(Object actual) {
+    this.expect(
+        Map::containsKey,
+        actual,
+        "expected {expected} to have keys which contain {actual}"
+    );
+
+    return this;
+  }
+
+  /**
+   * Expect our expected map's values to contain the provided actual value.
+   *
+   * @param actual the value we expect to be contained within our expected map's value collection
+   *
+   * @return self
+   */
+  public MapExpectation expectValuesContain(Object actual) {
+    this.expect(
+        Map::containsValue,
+        actual,
+        "expected {expected} to have values which contain {actual}"
+    );
+
+    return this;
+  }
+
+  /**
+   * Expect our expected map's keys do not match the provided actual key.
+   *
+   * @param actual the key we expect to not be contained within our expected map's key collection
+   *
+   * @return self
+   */
+  public MapExpectation expectKeysDoNotContain(Object actual) {
+    this.expect(
+        (e, a) -> !e.containsKey(a),
+        actual,
+        "expected {expected} to not have keys which contain {actual}"
+    );
+
+    return this;
+  }
+
+  /**
+   * Expect our expected map's values do not match the provided actual value.
+   *
+   * @param actual the value we expect to not be contained within our expected map's value
+   *               collection
+   *
+   * @return self
+   */
+  public MapExpectation expectValuesDoNotContain(Object actual) {
+    this.expect(
+        (e, a) -> !e.containsValue(a),
+        actual,
+        "expected {expected} to not have values which contain {actual}"
+    );
+
+    return this;
+  }
+
+  /**
+   * Expect our expected map's keys to match the given list of actual keys.
+   *
+   * @param actual the collection of keys we expect to be contained within our expected map
+   *
+   * @return self
+   */
+  public MapExpectation expectKeysContainAll(List<?> actual) {
+    this.expect(
+        (e, a) -> ((List<?>) a).stream().allMatch(e::containsKey),
+        actual,
+        "expected {expected} keys to contain all of {actual}"
+    );
+
+    return this;
+  }
+
+  /**
+   * Expect our expected map's keys to contain all the keys from the given actual map.
+   *
+   * @param actual the map whose keys we expect to be in our expected map's keys collection
+   *
+   * @return self
+   */
+  public MapExpectation expectKeysContainAll(Map<?, ?> actual) {
+    this.expectKeysContainAll(Arrays.asList(actual.keySet().toArray()));
+
+    return this;
+  }
+
+  /**
+   * Expect our expected map to contain all the provided actual value.
+   *
+   * @param actual the collection of values we expect to be contained within our expected map
+   *
+   * @return self
+   */
+  public MapExpectation expectValuesContainAll(List<?> actual) {
+    this.expect(
+        (e, a) -> ((List<?>) a).stream().allMatch(e::containsValue),
+        actual,
+        "expected {expected} values to contain all of {actual}"
+    );
+
+    return this;
+  }
+
+  /**
+   * Expect our expected map's values to contain all the values from the given actual map.
+   *
+   * @param actual the map whose values we expect to be in our expected map's values collection
+   *
+   * @return self
+   */
+  public MapExpectation expectValuesContainAll(Map<?, ?> actual) {
+    this.expectValuesContainAll(Arrays.asList(actual.values().toArray()));
+
+    return this;
+  }
+
+  /**
+   * Expect our expected map's keys to contain none of the keys in the provided actual list.
+   *
+   * @param actual the collection of keys we expect to not be contained within our expected
+   *               map's key collection
+   *
+   * @return self
+   */
+  public MapExpectation expectKeysDoNotContainAny(List<?> actual) {
+    this.expect(
+        (e, a) -> ((List<?>) a).stream().noneMatch(e::containsKey),
+        actual,
+        "expected the keys of {expected} to contain none of these keys: {actual}"
+    );
+
+    return this;
+  }
+
+  /**
+   * Expect our expected map's keys to contain none of the keys in the provided map's key
+   * list.
+   *
+   * @param actual a map whose keys we expect should not be found within our expected map's key
+   *               collection.
+   *
+   * @return self
+   */
+  public MapExpectation expectKeysDoNotContainAny(Map<?, ?> actual) {
+    this.expectKeysDoNotContainAny(Arrays.asList(actual.keySet().toArray()));
+
+    return this;
+  }
+
+  /**
+   * Expect our expected map's values to contain none of the items in the provided actual list.
+   *
+   * @param actual the collection of values we expect to not be contained within our expected
+   *               map's value collection
+   *
+   * @return self
+   */
+  public MapExpectation expectValuesDoNotContainAny(List<?> actual) {
+    this.expect(
+        (e, a) -> ((List<?>) a).stream().noneMatch(e::containsValue),
+        actual,
+        "expected the values in {expected} to contain none of these values: {actual}"
+    );
+
+    return this;
+  }
+
+  /**
+   * Expect our expected map's values to contain none of the values in the provided map's value
+   * list.
+   *
+   * @param actual a map whose values we expect should not be found within our expected map's value
+   *               collection.
+   *
+   * @return self
+   */
+  public MapExpectation expectValuesDoNotContainAny(Map<?, ?> actual) {
+    this.expectValuesDoNotContainAny(Arrays.asList(actual.values().toArray()));
+
+    return this;
+  }
+
+  /**
+   * Expect our expected map to be the given size.
+   *
+   * @param actual the size we expect
+   *
+   * @return self
+   */
+  public MapExpectation expectSize(Integer actual) {
+    this.expect(
+        (e, a) -> e.size() == (Integer) a,
+        actual,
+        "expected {expected} to have a size of {actual}"
+    );
+
+    return this;
+  }
+
+  /**
+   * Expect our expected map to be empty.
+   *
+   * @return self
+   */
+  public MapExpectation expectEmpty() {
+    this.expectSize(0);
+
+    return this;
+  }
+}
---
+++ expectator/src/test/java/org/cobhimself/expectator/expectations/types/MapExpectationTest.java
@@ -0,0 +1,181 @@
+package org.cobhimself.expectator.expectations.types;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+import org.cobhimself.expectator.exceptions.ExpectatorException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class MapExpectationTest
+    implements BaseExpectationTest<MapExpectation, Map<?,?>> {
+
+  final static Map<Integer, String> MAP_1_EXPECTED = Map.of(1, "one", 2, "two", 3, "three");
+  final static Map<String, Integer> MAP_2_EXPECTED = Map.of("four", 4, "five", 5, "six", 6);
+
+  final static String NAME = "Map expectation";
+
+  static MapExpectation getExpectation(Map<?, ?> map) {
+    return new MapExpectation(map, NAME);
+  }
+
+  @Test
+  void mainTest() {
+    assertEquals(
+        MAP_1_EXPECTED,
+        getExpectation(MAP_1_EXPECTED)
+            .getExpectedValue()
+    );
+
+    assertEquals(
+        MAP_2_EXPECTED,
+        getExpectation(MAP_2_EXPECTED)
+            .getExpectedValue()
+    );
+  }
+
+  @Test
+  void testConstructors() {
+    this.testExpectedValueAndNameConstructor(
+        new MapExpectation(MAP_1_EXPECTED, NAME),
+        MAP_1_EXPECTED,
+        NAME
+    );
+
+    this.testNameOnlyConstructor(new MapExpectation(NAME), NAME);
+  }
+
+  @ParameterizedTest()
+  @MethodSource("parameters")
+  void testExpectations(
+      Map<?,?> expected,
+      List<MapExpectation> willPass,
+      List<MapExpectation> willFail
+  ) {
+    var index = new AtomicInteger(1);
+    willPass.forEach(expectation -> {
+      expectation.setExpectedValue(expected);
+      assertDoesNotThrow(
+          expectation::confirm,
+          "pass example " + index.getAndIncrement() + ": "
+      );
+    });
+    index.set(1);
+    willFail.forEach(expectation -> {
+      expectation.setExpectedValue(expected);
+      assertThrows(
+          ExpectatorException.class,
+          expectation::confirm, "fail example " + index.getAndIncrement() + ": "
+      );
+    });
+  }
+
+  static Stream<Arguments> parameters() {
+    return Stream.of(
+        Arguments.of(
+            MAP_1_EXPECTED,
+            //Will Pass
+            List.of(
+                getExpectation(MAP_1_EXPECTED).expectValuesContain("one"),
+                getExpectation(MAP_1_EXPECTED).expectValuesContain("two"),
+                getExpectation(MAP_1_EXPECTED).expectKeysContain(1),
+                getExpectation(MAP_1_EXPECTED).expectKeysContain(2),
+                getExpectation(MAP_1_EXPECTED).expectValuesContainAll(List.of("one", "two")),
+                getExpectation(MAP_1_EXPECTED).expectValuesContainAll(Map.of(1, "one", 2, "two")),
+                getExpectation(MAP_1_EXPECTED).expectKeysContainAll(List.of(1, 2)),
+                getExpectation(MAP_1_EXPECTED).expectKeysContainAll(Map.of(1, "one", 2, "two")),
+                getExpectation(MAP_1_EXPECTED).expectSize(3),
+                getExpectation(MAP_1_EXPECTED).expectValuesDoNotContain(3),
+                getExpectation(MAP_1_EXPECTED).expectKeysDoNotContain("three"),
+                getExpectation(MAP_1_EXPECTED).expectValuesDoNotContainAny(List.of("four", "five")),
+                getExpectation(MAP_1_EXPECTED).expectValuesDoNotContainAny(
+                    Map.of(4, "four", 5, "five")
+                ),
+                getExpectation(MAP_1_EXPECTED).expectKeysDoNotContainAny(List.of(4, 5)),
+                getExpectation(MAP_1_EXPECTED).expectKeysDoNotContainAny(
+                    Map.of("four", 4, "five", 5)
+                )
+            ),
+            //Will Fail
+            List.of(
+                getExpectation(MAP_1_EXPECTED).expectEmpty(),
+                getExpectation(MAP_1_EXPECTED).expectEquals(Map.of(4, "one", 5, "two", 6, "three")),
+                getExpectation(MAP_1_EXPECTED).expectValuesContain("four"),
+                getExpectation(MAP_1_EXPECTED).expectKeysContain(4),
+                getExpectation(MAP_1_EXPECTED).expectValuesContain(1),
+                getExpectation(MAP_1_EXPECTED).expectKeysContain("one"),
+                getExpectation(MAP_1_EXPECTED).expectValuesContainAll(List.of("one", 1)),
+                getExpectation(MAP_1_EXPECTED).expectKeysContainAll(List.of("two", 4)),
+                getExpectation(MAP_1_EXPECTED).expectValuesContainAll(List.of("one", "two", "three", "four")),
+                getExpectation(MAP_1_EXPECTED).expectSize(0),
+                getExpectation(MAP_1_EXPECTED).expectValuesDoNotContain("one"),
+                getExpectation(MAP_1_EXPECTED).expectValuesDoNotContainAny(List.of("one", "three")),
+                getExpectation(MAP_1_EXPECTED).expectKeysDoNotContain(1),
+                getExpectation(MAP_1_EXPECTED).expectKeysDoNotContainAny(List.of(2, 3))
+            )
+        ),
+        Arguments.of(
+            MAP_2_EXPECTED,
+            //Will Pass
+            List.of(
+                getExpectation(MAP_2_EXPECTED).expectValuesContain(4),
+                getExpectation(MAP_2_EXPECTED).expectValuesContain(5),
+                getExpectation(MAP_2_EXPECTED).expectKeysContain("four"),
+                getExpectation(MAP_2_EXPECTED).expectKeysContain("five"),
+                getExpectation(MAP_2_EXPECTED).expectValuesContainAll(List.of(5, 6)),
+                getExpectation(MAP_2_EXPECTED).expectValuesContainAll(Map.of("four", 4, "five", 5)),
+                getExpectation(MAP_2_EXPECTED).expectKeysContainAll(List.of("four", "five")),
+                getExpectation(MAP_2_EXPECTED).expectKeysContainAll(Map.of("four", 4, "five", 5)),
+                getExpectation(MAP_2_EXPECTED).expectSize(3),
+                getExpectation(MAP_2_EXPECTED).expectValuesDoNotContain("three"),
+                getExpectation(MAP_2_EXPECTED).expectKeysDoNotContain(3),
+                getExpectation(MAP_2_EXPECTED).expectValuesDoNotContainAny(List.of(1, 2)),
+                getExpectation(MAP_2_EXPECTED).expectValuesDoNotContainAny(
+                    Map.of("one", 2, "two", 3)
+                ),
+                getExpectation(MAP_2_EXPECTED).expectKeysDoNotContainAny(List.of("one", "two")),
+                getExpectation(MAP_2_EXPECTED).expectKeysDoNotContainAny(
+                    Map.of(4, "four", 5, "five")
+                )
+            ),
+            //Will Fail
+            //MAP_2_EXPECTED = Map.of("four", 4, "five", 5, "six", 6);
+            List.of(
+                getExpectation(MAP_2_EXPECTED).expectEmpty(),
+                getExpectation(MAP_2_EXPECTED).expectEquals(Map.of(4, "four", 5, "five", 6, "six")),
+                getExpectation(MAP_2_EXPECTED).expectValuesContain(1),
+                getExpectation(MAP_2_EXPECTED).expectKeysContain("one"),
+                getExpectation(MAP_2_EXPECTED).expectValuesContain(1),
+                getExpectation(MAP_2_EXPECTED).expectKeysContain("one"),
+                getExpectation(MAP_2_EXPECTED).expectValuesContainAll(List.of("one", 1)),
+                getExpectation(MAP_2_EXPECTED).expectKeysContainAll(List.of("two", 4)),
+                getExpectation(MAP_2_EXPECTED).expectValuesContainAll(List.of("one", "two", "three", "four")),
+                getExpectation(MAP_2_EXPECTED).expectSize(0),
+                getExpectation(MAP_2_EXPECTED).expectValuesDoNotContain(4),
+                getExpectation(MAP_2_EXPECTED).expectValuesDoNotContainAny(List.of(5, 6)),
+                getExpectation(MAP_2_EXPECTED).expectKeysDoNotContain("four"),
+                getExpectation(MAP_2_EXPECTED).expectKeysDoNotContainAny(List.of("five", "six"))
+            )
+        ),
+        Arguments.of(
+            Collections.emptyMap(),
+            //Will Pass
+            List.of(
+                getExpectation(Collections.emptyMap()).expectEmpty()
+            ),
+            //Will Fail
+            List.of()
+        )
+    );
+  }
+}
